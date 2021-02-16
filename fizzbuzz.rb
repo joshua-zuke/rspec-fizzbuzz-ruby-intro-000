3ddb6766322ad765c342aaa18e9122ff44cfe08d
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-16 19:15:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(input)
-  if input % 3==0 # First mistake I made was using "=" and not "=="
-    return "Fizz" #Second mistake was putting "puts" and not "return" meaning I got nil in all cases.
+  if input % 3==0 && input % 5==0 # First mistake I made was using "=" and not "=="
+    return "FizzBuzz" #Second mistake was putting "puts" and not "return" meaning I got nil in all cases.
   elsif input %5 ==0
     return "Buzz"
-  elsif input ==15
-    return "FizzBuzz"
+  elsif input % 3==0
+    return "Fizz"
   end
 end #Third mistake was not having an "end" here to end the definition in addition to conditional end.
 
