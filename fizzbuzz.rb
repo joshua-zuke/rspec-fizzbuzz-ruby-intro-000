48bc0c970e8e6885bb4c19a781df0edc60c82e5b
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-16 19:08:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(input)
-  if input==3 # First mistake I made was using "=" and not "=="
+  if input % 3==0 # First mistake I made was using "=" and not "=="
     return "Fizz" #Second mistake was putting "puts" and not "return" meaning I got nil in all cases.
-  elsif input==5
+  elsif input %5 ==0
     return "Buzz"
   elsif input==15
     return "FizzBuzz"
   end
 end #Third mistake was not having an "end" here to end the definition in addition to conditional end.
+
+# Note: Originally I cheated by hardcoding values like if input = 3 return Fizz.
+# That means that fizzbuzz(6) would return nil even though it's divisible by 3.
