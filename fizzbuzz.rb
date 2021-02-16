78c15d4fe505b9e5c69b7761fc6ec51d5f070e6c
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-16 19:19:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,7 @@ def fizzbuzz(input)
     return "Buzz"
   elsif input % 3==0
     return "Fizz"
+    puts "Fizz"
   end
 end #Third mistake was not having an "end" here to end the definition in addition to conditional end.
 
